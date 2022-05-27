0212fadcc6c6337ed7be9420161dc15fe839308d
Kolya <you@example.com>
2022-05-27 13:31:37 +0300

    seven commit

--- tasks3/tasks_1-12.js
+++ tasks3/tasks_1-12.js
@@ -319,6 +319,9 @@
     for (const key in object) {
       if (Object.hasOwnProperty.call(object, key)) {
         const element = object[key];
+        console.log(element);
+
+        propCount += 1;
       }
     }
     return propCount;
